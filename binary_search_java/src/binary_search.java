8b53fc242a6d635b0ab8822549767f4e2e2ffea6
Sahil-Gupta582 <56026206+Sahil-Gupta582@users.noreply.github.com>
2020-09-26 01:38:21 +0530

    Condition to exit if array is not sorted
    Slight Modifications

--- binary_search_java/src/binary_search.java
+++ binary_search_java/src/binary_search.java
@@ -57,13 +57,37 @@ class Solution
         int n=in.nextInt();
         System.out.println("Enter the elements of array");
         int[] arr=in.readArray(n);
-        Arrays.sort(arr);
-        System.out.println("Enter the element to be searched");
-        int ele=in.nextInt();
-        int pos=BinarySearch(arr,ele);
-        out.println(pos);
+        boolean isSorted=checkSort(arr);
+        if(isSorted){
+            Arrays.sort(arr);
+            System.out.println("Enter the element to be searched");
+            int ele=in.nextInt();
+            int pos=BinarySearch(arr,ele);
+            out.println(pos);
+        }
+        else
+        out.println("Array not Sorted");
     }
-
+    public static boolean checkSort(int[] arr)
+    {
+        boolean res=true;
+        boolean res2=true;
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1])
+            {
+                res=false;
+                break;
+            }
+        }
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]<arr[i+1])
+            {
+                res2=false;
+                break;
+            }
+        }
+        return res||res2;
+    }
     public static int BinarySearch(int[] arr, int ele)
     {
         int high=arr.length-1;
@@ -74,9 +98,9 @@ class Solution
             if(arr[mid]==ele)
                 return mid;
             if(arr[mid]>ele)
-                low=mid+1;
-            else
                 high=mid-1;
+            else
+                low=mid+1;
         }
         return -1;
     }

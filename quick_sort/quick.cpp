e47665fca83780beb61df7ceb42a500b5e4ff2a7
SHAURY SRIVASTAV <66357200+shaurysrivastav27@users.noreply.github.com>
2020-09-26 11:39:14 +0530

    added the comments
    added the comments in the code

--- quick_sort/quick.cpp
+++ quick_sort/quick.cpp
@@ -2,26 +2,26 @@
 using namespace std;
 int partition(int a[],int l,int r)
 {
-    int  p = a[r];
-    int  k = l-1;
+    int  p = a[r];                       //assigning the initial index
+    int  k = l-1;						//assigning the max index 
     for(int i= l;i<r;i++)
     {
-        if(a[i]<p)
-        {
+        if(a[i]<p)						//searching for min value 
+        {						
             k++;
-            swap(a[k],a[i]);
+            swap(a[k],a[i]);			//swapping with the proper element
         }
     }
-    swap(a[k+1],a[r]);
+    swap(a[k+1],a[r]);					//element to be swapped with a[r] 
     return k+1;
 }
-void quick(int a[],int l ,int r)
+void quick(int a[],int l ,int r)        //function to call recursively the quick sort algo
 {
     if(l<=r)
     {
-        int p = partition(a,l,r);
-        quick(a,l,p-1);
-        quick(a,p+1,r);
+        int p = partition(a,l,r);		//calling the partition function recursively to find the partition index
+        quick(a,l,p-1);					//sorting the first half recursively 
+        quick(a,p+1,r);					//sortng the second half recursively 
     }
 }
 int main()

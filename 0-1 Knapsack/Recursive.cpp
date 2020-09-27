634aeea2a3f79220821d041676479c2b08aeba7e
Sanskar Seth <55178412+sanskarseth@users.noreply.github.com>
2020-09-27 12:17:07 +0530

    Update Recursive.cpp
    removed extra blank lines

--- 0-1 Knapsack/Recursive.cpp
+++ 0-1 Knapsack/Recursive.cpp
@@ -1,31 +1,23 @@
-
-Recursive approach for 0-1 Knapsack
-
+/* Recursive approach for 0-1 Knapsack */
 
 #include <bits/stdc++.h>
 using namespace std;
 
 
-
 // Function to return the maimum profit
 int knapsack ( int *wt , int *value , int n , int cap){
-    
-    
+        
     //Base condtions
     if( n<=0  || cap<=0)
-    return 0;
-    
+    	return 0;
     
     /* A particular object might get selected in the knapsack
        only if it's weight is less than the capacity of knapsack*/
        
     if(wt[n-1]<=cap)
-    return max( knapsack(wt,value,n-1,cap) , value[n-1] + knapsack(wt,value,n-1,cap-wt[n-1]));
-    
+    	return max( knapsack(wt,value,n-1,cap) , value[n-1] + knapsack(wt,value,n-1,cap-wt[n-1]));
     
     return  knapsack(wt,value,n-1,cap);
- 
- 
  
 }
 
@@ -33,10 +25,9 @@ int main() {
     
      ios_base::sync_with_stdio(false);
      cin.tie(NULL);
-     
-    
+        
       int cap,i,n;
-      
+	
       cin>>n;
       
       int wt[n],value[n];
@@ -48,19 +39,6 @@ int main() {
          cin>>value[i];
       
       cin>>cap;
-      
-      
-      cout<< knapsack(wt , value , n ,cap)<<endl;
-  
-	
+ 
+      cout<< knapsack(wt , value , n ,cap)<<endl;	
 }
-
-
-
-
-
-
-
-
-
-

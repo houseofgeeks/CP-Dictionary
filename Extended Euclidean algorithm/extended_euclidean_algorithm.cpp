7652d881d13dcf06ac666c9182093e3686274d2c
itskeffect <kashishgupta9900@gmail.com>
2020-10-03 16:34:44 +0530

    Create extended_euclidean_algorithm.cpp

---
+++ Extended Euclidean algorithm/extended_euclidean_algorithm.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int extendedEuclidean(int a,int b,int &x,int &y){
+    if(a==0){
+        x=0;
+        y=1;
+        return b;
+    }
+    int x1,y1;
+    int gcd = extendedEuclidean(b%a,a,x1,y1);
+    x=y1-(b/a)*x1;
+    y=x1;
+    return 0;
+}
+int main(){
+    int a = 60,b=4,x,y;
+    int gcd= extendedEuclidean(a,b,x,y);
+    cout<<gcd<<endl;
+    return 0;
+}
+
+//ax + by = gcd and (b%a)x1 + ay1 = gcd (by Euclidean Algorithm) 
+//solving these equation we get x = y1 -(b/a)*x1 and y = x1

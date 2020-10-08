53238af3402e5e46a9390b1763b5933f65c16d6a
ricky1812 <rickydas189@gmail.com>
2020-10-08 08:57:41 +0530

    addedchanges

--- Scramble Strings/ScrambleString.cpp
+++ Scramble Strings/ScrambleString.cpp
@@ -4,7 +4,7 @@ using namespace std;
 unordered_map<string,int>mp;
     bool scramble(string s1,string s2)
     {
-       //cout<<s1<<" "<<s2<<"/";
+      
         if(s1==s2)
             return true;
         if(s1.length()<=1)
@@ -13,12 +13,12 @@ unordered_map<string,int>mp;
         if(mp.find(key)!=mp.end())
             return mp[key];
         int n=s1.length();
-       // cout<<key;
+      
         bool flag=false;
         for(int i=1;i<=n-1;i++)
         {
-            cout<<s1.substr(0,i)<<" "<<s2.substr(n-i,i)<<"/";
-           // cout<<s1.substr(i,n-i)<<" "<<s2.substr(0,n-i)<<"/";
+           
+          
             if((scramble(s1.substr(0,i),s2.substr(n-i,i)) && 
               scramble(s1.substr(i,n-i),s2.substr(0,n-i)) )||
                (scramble(s1.substr(0,i),s2.substr(0,i)) && 

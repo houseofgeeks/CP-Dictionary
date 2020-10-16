b9ebece0ed959e4869629e2da2956d4e8c021ab6
Sanjeev Singh <60501204+NobodySS07@users.noreply.github.com>
2020-10-16 22:14:32 +0530

    Update Insertion_sort.cpp

--- Insertion_Sort/Insertion_sort.cpp
+++ Insertion_Sort/Insertion_sort.cpp
@@ -3,7 +3,8 @@
 using namespace std;
  
 template <class T>
-void Print(T& vec, int n, string s){
+void Print(T& vec, int n, string s)     // A utility function to print an array of size n 
+{
     cout << s << ": [" << flush;
     for (int i=0; i<n; i++){
         cout << vec[i] << flush;
@@ -14,11 +15,13 @@ void Print(T& vec, int n, string s){
     cout << "]" << endl;
 }
  
-void InsertionSort(int A[], int n){
+void InsertionSort(int A[], int n)    /*function for insertion Sorting , here A is the array to be sorted  , n is the size of the array */
+{
     for (int i=1; i<n; i++){
         int j = i-1;
         int x = A[i];
-        while (j>-1 && A[j] > x){
+        while (j>-1 && A[j] > x)     /* Move elements of arr[0..i-1], that are greater than key, to one position ahead of their current position */
+        {
             A[j+1] = A[j];
             j--;
         }
@@ -26,13 +29,14 @@ void InsertionSort(int A[], int n){
     }
 }
  
-int main() {
+int main() 
+{
  
-    int A[] = {19, 17, 15, 13, 11, 9, 7, 5, 3, 1};
-    Print(A, sizeof(A)/sizeof(A[0]), "       A");
+    int A[] = {121,32,434,565,5767,7654,56567};  //predifined array
+    Print(A, sizeof(A)/sizeof(A[0]), "       A");   //printing unsorted Array
  
-    InsertionSort(A, sizeof(A)/sizeof(A[0]));
-    Print(A, sizeof(A)/sizeof(A[0]), "Sorted A");
+    InsertionSort(A, sizeof(A)/sizeof(A[0])); //calling insertion sort
+    Print(A, sizeof(A)/sizeof(A[0]), "Sorted A"); //printing sorted Array
  
     return 0;
 }
